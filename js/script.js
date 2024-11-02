69767707c965bb6c3255f7edb324e74844facb78
yeon44 <jangyeon33@gmail.com>
2024-11-02 18:34:14 +0900

    1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+let ex_01 = document.querySelector("#test_01Img");
 
+let ex_01Img = function () {
+  ex_01.setAttribute("src", "img/smurfs_img.jpg");
+  ex_01.setAttribute("alt", "스머프포스터");
+  ex_01.style.width = "160px";
+};
+
+ex_01Img();
