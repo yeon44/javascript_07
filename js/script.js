f7421aa06037f8c6974532701b199c0e1c05fcef
yeon44 <jangyeon33@gmail.com>
2024-11-08 23:55:17 +0900

    중단

--- js/script.js
+++ js/script.js
@@ -8,13 +8,28 @@ let ex_01Img = function () {
 // => 호출
 ex_01Img();
 
-let ex_02arr = [
-  {
-    img: "./img/smurf_movie.png",
-    alt: "스머프의 모험",
-  },
-];
+// let ex_02 = document.querySelector("div"); // => div 태그 선택
 
-let ex_02 = document.querySelector("div"); // => div 태그 선택
+// let ex_02arr = [
+//   {
+//     img: "./img/smurf_movie.png",
+//     alt: "스머프 사진",
+//     text: "스머프의 모험",
+//   },
+// ];
 
-function ex_02Img(img, alt) {}
+// function ex_02Poster(img) {
+//   let fnc_02 = img;
+//   return fnc_02;
+// }
+
+// for (v of ex_02arr) {
+//   ex_02Poster;
+
+// }
+
+// let ex_02Rsult = ex_02Poster(ex_02arr.img);
+// console.log(`./img/smurf_movie.png`);
+// document.write(ex_02Rsult);
+
+const ex_03 = document.getElementById("#test_03");
